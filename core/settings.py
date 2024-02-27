b35c17df80f3074d0411e23a6a7885ade2d903e3
zozulia.kirill <kirillzozulya95@gmail.com>
2024-02-27 14:32:43 +0200

    update settings

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'developer95.pythonanywhere.com']
 
 
 # Application definition
